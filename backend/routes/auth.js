75a0f4557b4eeed4ecf46835457604320878c6e7
Manish Pandey <119288527+manishpandeyprojects@users.noreply.github.com>
2023-01-02 18:50:42 +0530

    MP: Send request as json and saved the data..

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    obj = {
-        name: "sdbdjv",
-        number: 54, 
-    }
-    res.json(obj);
+router.post('/', (req, res)=>{
+    console.log(req.body);
+    
+    const user = User(req.body);
+    user.save(); 
+
+    res.send(req.body);
+
 })
 
 module.exports = router;
